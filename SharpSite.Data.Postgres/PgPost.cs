6d582bd5265f1ab501b33d22a6ec1d73a239d610
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2024-10-15 09:59:10 -0400

    WIP

---
+++ SharpSite.Data.Postgres/PgPost.cs
@@ -0,0 +1,49 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace SharpSite.Data.Postgres;
+
+/// <summary>
+/// A postgres specific implementation of a post.
+/// </summary>
+public class PgPost
+{
+
+	[Required]
+	public required string Slug { get; set; }
+
+	[Required, MaxLength(300)]
+	public required string Title { get; set; }
+
+	[Required]
+	public required string Content { get; set; } = string.Empty;
+
+	[Required]
+	public required DateTimeOffset Published { get; set; } = DateTimeOffset.MaxValue;
+
+	public static explicit operator PgPost(SharpSite.Abstractions.Post post)
+	{
+
+		return new PgPost
+		{
+			Slug = post.Slug,
+			Title = post.Title,
+			Content = post.Content,
+			Published = post.PublishedDate
+		};
+
+	}
+
+	public static explicit operator SharpSite.Abstractions.Post(PgPost post)
+	{
+
+		return new SharpSite.Abstractions.Post
+		{
+			Slug = post.Slug,
+			Title = post.Title,
+			Content = post.Content,
+			PublishedDate = post.Published
+		};
+
+	}
+
+}
