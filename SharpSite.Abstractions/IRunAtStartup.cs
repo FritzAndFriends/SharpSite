14440438a250e3d390282e5ff0139f4bcf61ea3a
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2024-12-05 12:49:27 -0600

    Adding authentication and initial authorization (#99)

---
+++ SharpSite.Abstractions/IRunAtStartup.cs
@@ -0,0 +1,14 @@
+﻿namespace SharpSite.Abstractions;
+
+/// <summary>
+/// Interface for services that need to run at startup of the web application.
+/// </summary>
+public interface IRunAtStartup
+{
+	Task RunAtStartup(IServiceProvider services);
+}
+
+public interface IHasEndpoints
+{
+	void MapEndpoints(IServiceProvider services);
+}
