0a52039328f99f95e1ffe68d68368e304324674f
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2024-12-05 12:49:27 -0600

    Adding authentication and initial authorization (#99)

---
+++ SharpSite.Abstractions/SharpSiteUser.cs
@@ -0,0 +1,15 @@
+﻿namespace SharpSite.Abstractions;
+
+public class SharpSiteUser
+{
+	public SharpSiteUser(string id, string? userName, string? email)
+	{
+		Id = id;
+		UserName = userName;
+		Email = email;
+	}
+
+	public string Id { get; }
+	public string? UserName { get; }
+	public string? Email { get; }
+}
