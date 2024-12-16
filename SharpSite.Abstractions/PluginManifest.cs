b4838b31089653cea11e566ab27e2169b49a035a
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2024-12-16 14:23:12 -0500

    First attempt at theme chooser (#143)

--- SharpSite.Abstractions/PluginManifest.cs
+++ SharpSite.Abstractions/PluginManifest.cs
@@ -17,12 +17,17 @@ public class PluginManifest
 	public string? Source { get; set; }
 	public string? KnownLicense { get; set; }
 	public string[]? Tags { get; set; }
-	public required string[] Features { get; set; }
+	public required PluginFeatures[] Features { get; set; }
 
 	public string IdVersionToString()
 	{
 		return $"{id}@{Version}";
 	}
 
+}
+
+public enum PluginFeatures
+{
+	Theme
 }
 
