6a319264e8da8195cc09fbc03dd1f027e207396b
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2024-12-17 11:01:11 -0500

    0.4 release train (#101)
    Co-authored-by: Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
    Co-authored-by: Michael de Bruin <michael.bruin@live.nl>
    Co-authored-by: default-writer <funcelot@outlook.com>
    Co-authored-by: Tero Kilpeläinen <48437506+degenone@users.noreply.github.com>
    Co-authored-by: MdeBruin <mdebruin93@gmail.com>

---
+++ Sample.FirstThemePlugin/MyTheme.cs
@@ -0,0 +1,12 @@
+﻿using SharpSite.Abstractions.Theme;
+
+namespace Sample.FirstThemePlugin;
+
+public class MyTheme : IHasStylesheets
+{
+
+	public string[] Stylesheets => [
+		"theme.css"
+	];
+
+}
