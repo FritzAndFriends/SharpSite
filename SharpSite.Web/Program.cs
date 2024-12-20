b48c8466ae7fc902da33b42aac0a9d089a69c3a6
default-writer <funcelot@outlook.com>
2024-12-20 20:10:45 +0300

    Added SharpSite.Plugins project and refactored plugin system (#169)
    Co-authored-by: Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>

--- SharpSite.Web/Program.cs
+++ SharpSite.Web/Program.cs
@@ -1,10 +1,11 @@
-using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.FileProviders;
 using SharpSite.Data.Postgres;
 using SharpSite.Security.Postgres;
 using SharpSite.Web;
 using SharpSite.Web.Components;
 using SharpSite.Web.Locales;
+using SharpSite.Plugins;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,6 +24,7 @@ pgSecurity.RegisterServices(builder);
 var appState = new ApplicationState();
 await appState.Load();
 builder.Services.AddSingleton(appState);
+builder.Services.AddSingleton<PluginAssemblyManager>();
 builder.Services.AddSingleton<PluginManager>();
 
 // add the custom localization features for the application framework
@@ -30,7 +32,6 @@ builder.ConfigureRequestLocalization();
 
 // Add service defaults & Aspire components.
 builder.AddServiceDefaults();
-
 // Configure larger messages to allow upload of packages
 builder.Services.Configure<HubOptions>(options =>
 {
@@ -96,6 +97,6 @@ await pgSecurity.RunAtStartup(app.Services);
 
 // Use DI to get the logger
 var pluginManager = app.Services.GetRequiredService<PluginManager>();
-pluginManager.LoadPluginsAtStartup();
+await pluginManager.LoadPluginsAtStartup();
 
 app.Run();
