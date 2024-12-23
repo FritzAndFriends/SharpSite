a63710683a0e64a43c9498928029e0fbc170c575
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2024-12-23 15:38:12 -0500

    Finished naive implementation of filesystem storage

--- SharpSite.Plugins.FileStorage.FileSystem/FileSystemStorage.cs
+++ SharpSite.Plugins.FileStorage.FileSystem/FileSystemStorage.cs
@@ -8,6 +8,14 @@ public class FileSystemStorage(FileSystemConfigurationSection configuration) : I
 	public async Task AddFile(FileData file)
 	{
 
+		ArgumentNullException.ThrowIfNull(file, nameof(file));
+		if (file.File is null || file.File.Length == 0)
+		{
+			throw new ArgumentException("Missing file", nameof(file));
+		}
+
+		file.Metadata.ValidateFileName();
+
 		var path = Path.Combine(configuration.BaseFolderName, file.Metadata.FileName);
 		using var fileStream = File.Create(path);
 		await file.File.CopyToAsync(fileStream);
@@ -16,16 +24,36 @@ public class FileSystemStorage(FileSystemConfigurationSection configuration) : I
 
 	public Task<FileData> GetFile(string filename)
 	{
-		throw new NotImplementedException();
+
+		// get the file from disk and return it with metadata
+		var path = Path.Combine(configuration.BaseFolderName, filename);
+
+		var memoryStream = new MemoryStream();
+		using (var file = File.Open(path, FileMode.Open))
+		{
+			file.CopyTo(memoryStream);
+			memoryStream.Position = 0;
+		}
+		var metadata = new FileMetaData(filename, File.GetCreationTime(path));
+		return Task.FromResult(new FileData(memoryStream, metadata));
 	}
 
 	public Task<IEnumerable<FileMetaData>> GetFiles(int page, int filesOnPage, out int totalFilesAvailable)
 	{
-		throw new NotImplementedException();
+
+		var files = Directory.GetFiles(configuration.BaseFolderName);
+		totalFilesAvailable = files.Length;
+		var filesOnPageArray = files.Skip((page - 1) * filesOnPage).Take(filesOnPage).Select(f => new FileMetaData(Path.GetFileName(f), File.GetCreationTime(f)));
+		return Task.FromResult(filesOnPageArray);
+
 	}
 
 	public Task RemoveFile(string filename)
 	{
-		throw new NotImplementedException();
+
+		var path = Path.Combine(configuration.BaseFolderName, filename);
+		File.Delete(path);
+		return Task.CompletedTask;
+
 	}
 }
