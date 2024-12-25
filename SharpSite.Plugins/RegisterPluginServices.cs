8822a13b445865189c817c736f3c3c2146550df5
default-writer <funcelot@outlook.com>
2024-12-25 18:24:13 +0300

    merged from v0.5

---
+++ SharpSite.Plugins/RegisterPluginServices.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SharpSite.Abstractions;
+
+namespace SharpSite.Plugins;
+
+public class RegisterPluginServices : IRegisterServices, IRunAtStartup
+{
+	public IHostApplicationBuilder RegisterServices(IHostApplicationBuilder builder, bool disableRetry = false)
+	{
+		builder.Services.AddSingleton<IPluginAssemblyManager, PluginAssemblyManager>();
+		builder.Services.AddSingleton<IPluginManager, PluginManager>();
+		builder.Services.AddSingleton<IThemeManager, ThemeManager>();
+		return builder;
+	}
+
+	public async Task RunAtStartup(IServiceProvider services)
+	{
+		var pluginManager = services.GetRequiredService<IPluginManager>();
+		await pluginManager.LoadAtStartup();
+
+		var themeManager = services.GetRequiredService<IThemeManager>();
+		await themeManager.LoadAtStartup();
+	}
+}
