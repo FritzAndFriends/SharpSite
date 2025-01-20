e7f784cca6d9e58a1aae978a4dfcf830287eefcb
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2025-01-20 12:46:09 -0500

    Added a foundation to start writing and running unit tests (#241)

---
+++ tests/SharpSite.Tests.Web/ApplicationState/SetConfigurationSection/GivenNullSection.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+
+namespace SharpSite.Tests.Web.ApplicationState.SetConfigurationSection;
+
+public class GivenNullSection : BaseFixture
+{
+
+	[Fact]
+	public void WhenSettingNullSection_ThenThrowsArgumentNullException()
+	{
+		// Act
+		Action act = () => ApplicationState.SetConfigurationSection(null!);
+
+		// Assert
+		Assert.Throws<ArgumentNullException>(act);
+
+	}
+}
