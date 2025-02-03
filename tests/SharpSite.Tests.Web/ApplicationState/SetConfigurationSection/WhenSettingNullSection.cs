090f5db60ed990472f3f203c6439b0b27c3d56fa
Tero Kilpeläinen <degenone1@gmail.com>
2025-02-03 19:46:18 +0200

    Fix test indentation

--- tests/SharpSite.Tests.Web/ApplicationState/SetConfigurationSection/WhenSettingNullSection.cs
+++ tests/SharpSite.Tests.Web/ApplicationState/SetConfigurationSection/WhenSettingNullSection.cs
@@ -5,13 +5,13 @@ namespace SharpSite.Tests.Web.ApplicationState.SetConfigurationSection;
 
 public class WhenSettingNullSection : BaseFixture
 {
-    [Fact]
-    public async Task ThenThrowsArgumentNullException()
-    {
-        // Act
-        async Task Act() => await ApplicationState.SetConfigurationSection(null!);
+	[Fact]
+	public async Task ThenThrowsArgumentNullException()
+	{
+		// Act
+		async Task Act() => await ApplicationState.SetConfigurationSection(null!);
 
-        // Assert
-        await Assert.ThrowsAsync<ArgumentNullException>(Act);
-    }
+		// Assert
+		await Assert.ThrowsAsync<ArgumentNullException>(Act);
+	}
 }
