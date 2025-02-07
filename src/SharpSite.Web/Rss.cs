111eb08e12b1d4834d06776bccc5b43075d12ed5
Tero Kilpeläinen <degenone1@gmail.com>
2025-02-07 15:28:10 +0200

    Add test for RSS feed

--- src/SharpSite.Web/Rss.cs
+++ src/SharpSite.Web/Rss.cs
@@ -19,47 +19,53 @@ public static class Program_Rss
 		app.MapGet("/rss.xml", async (HttpContext context, IPostRepository postRepository) =>
 		{
 
-			var sb = new StringBuilder();
-			sb.AppendLine("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
-			sb.AppendLine("<rss version=\"2.0\">");
-			sb.AppendLine("<channel>");
-			sb.AppendLine("<title>SharpSite</title>");
-			
-			// generate the feed link from the current request using the scheme and host name
-			sb.AppendLine($"<link>{context.Request.Scheme}://{context.Request.Host}</link>");
-
 			var posts = await postRepository.GetPosts();
-
-			foreach (var post in posts)
-			{
-
-				// generate the post link from the current request using the scheme and host name
-				var postLink = $"{context.Request.Scheme}://{context.Request.Host}{post.ToUrl()}";
-				// add the post to the stringbuilder xml document
-				sb.AppendLine("<item>");
-				sb.AppendLine($"<title>{SecurityElement.Escape(post.Title)}</title>");
-				sb.AppendLine($"<link>{postLink}</link>");
-				sb.AppendLine($"<description>{SecurityElement.Escape(post.Description)}</description>");
-				sb.AppendLine($"<pubDate>{post.PublishedDate.ToString("R")}</pubDate>");
-				sb.AppendLine("</item>");
-
-			}
-
-			sb.AppendLine("</channel>");
-			sb.AppendLine("</rss>");
 
 			context.Response.StatusCode = (int)HttpStatusCode.OK;
 			context.Response.ContentType = "application/rss+xml";
-			await context.Response.WriteAsync(sb.ToString());
+			await context.Response.WriteAsync(GenerateRSS($"{context.Request.Scheme}://{context.Request.Host}", posts));
 
-			}).CacheOutput(policy =>
-			{
-				policy.Tag("rss");
-				policy.Expire(TimeSpan.FromMinutes(30));
-			});
+		}).CacheOutput(policy =>
+		{
+			policy.Tag("rss");
+			policy.Expire(TimeSpan.FromMinutes(30));
+		});
 
 		return app;
 
 	}
 
+	internal static string GenerateRSS(string urlBase, IEnumerable<Post> posts)
+	{
+
+		var sb = new StringBuilder();
+		sb.AppendLine("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+		sb.AppendLine("<rss version=\"2.0\">");
+		sb.AppendLine("<channel>");
+		sb.AppendLine("<title>SharpSite</title>");
+
+		// generate the feed link from the current request using the scheme and host name
+		sb.AppendLine($"<link>{urlBase}</link>");
+
+		foreach (var post in posts)
+		{
+
+			// generate the post link from the current request using the scheme and host name
+			var postLink = $"{urlBase}{post.ToUrl()}";
+			// add the post to the stringbuilder xml document
+			sb.AppendLine("<item>");
+			sb.AppendLine($"<title>{SecurityElement.Escape(post.Title)}</title>");
+			sb.AppendLine($"<link>{postLink}</link>");
+			sb.AppendLine($"<description>{SecurityElement.Escape(post.Description)}</description>");
+			sb.AppendLine($"<pubDate>{post.PublishedDate:R}</pubDate>");
+			sb.AppendLine("</item>");
+
+		}
+
+		sb.AppendLine("</channel>");
+		sb.AppendLine("</rss>");
+
+		return sb.ToString();
+
+	}
 }
---
+++ tests/SharpSite.Tests.Web/RSS/GenerateRSS.cs
@@ -0,0 +1,60 @@
+﻿using SharpSite.Abstractions;
+using SharpSite.Web;
+using System.Security;
+using Xunit;
+
+namespace SharpSite.Tests.Web.RSS;
+
+public class GenerateRss
+{
+	[Fact]
+	public void ReturnsValidRssFormat()
+	{
+
+		// Arrange
+		var urlBase = "http://localhost";
+		IEnumerable<Post> posts = [
+			new Post
+			{
+				Slug = "test-post-1",
+				Title = "Test Post 1",
+				Description = "Description for Test Post 1",
+				Content = "Content for Test Post 1",
+				PublishedDate = DateTimeOffset.Now,
+				LastUpdate = DateTimeOffset.Now,
+				LanguageCode = "en"
+			},
+			new Post
+			{
+				Slug = "test-post-2",
+				Title = "Test Post 2",
+				Description = "Description for Test Post 2",
+				Content = "Content for Test Post 2",
+				PublishedDate = DateTimeOffset.Now,
+				LastUpdate = DateTimeOffset.Now,
+				LanguageCode = "en"
+			}
+		];
+
+		// Act
+		var rss = Program_Rss.GenerateRSS(urlBase, posts);
+
+		// Assert
+		Assert.Contains("<?xml version=\"1.0\" encoding=\"utf-8\"?>", rss);
+		Assert.Contains("<rss version=\"2.0\">", rss);
+		Assert.Contains("<channel>", rss);
+		Assert.Contains("<title>SharpSite</title>", rss);
+		Assert.Contains("<link>http://localhost</link>", rss);
+		foreach (var post in posts)
+		{
+			Assert.Contains($"<title>{SecurityElement.Escape(post.Title)}</title>", rss);
+			Assert.Contains($"<link>http://localhost{post.ToUrl()}</link>", rss);
+			Assert.Contains($"<description>{SecurityElement.Escape(post.Description)}</description>", rss);
+			Assert.Contains($"<pubDate>{post.PublishedDate:R}</pubDate>", rss);
+		}
+		Assert.Contains("</channel>", rss);
+		Assert.Contains("</rss>", rss);
+
+	}
+}
+
