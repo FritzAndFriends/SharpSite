c01f1b9af158864d8a3f24e81e6797776941f2fa
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2025-02-13 10:55:28 -0500

    Added an Aspire command to run playwright tests

--- src/SharpSite.AppHost/Program.cs
+++ src/SharpSite.AppHost/Program.cs
@@ -4,14 +4,14 @@ var testOnly = false;
 
 foreach (var arg in args)
 {
-    if (arg.StartsWith("--testonly"))
-    {
-        var parts = arg.Split('=');
-        if (parts.Length == 2 && bool.TryParse(parts[1], out var result))
-        {
-            testOnly = result;
-        }
-    }
+	if (arg.StartsWith("--testonly"))
+	{
+		var parts = arg.Split('=');
+		if (parts.Length == 2 && bool.TryParse(parts[1], out var result))
+		{
+			testOnly = result;
+		}
+	}
 }
 
 var (db, migrationSvc) = builder.AddPostgresServices(testOnly);
@@ -19,6 +19,7 @@ var (db, migrationSvc) = builder.AddPostgresServices(testOnly);
 builder.AddProject<Projects.SharpSite_Web>("webfrontend")
 	.WithReference(db)
 	.WaitForCompletion(migrationSvc)
+	.WithRunE2eTestsCommand()
 	.WithExternalHttpEndpoints();
 
 if (testOnly)
@@ -46,7 +47,9 @@ if (testOnly)
 	Console.WriteLine("Starting the site in test mode");
 	await theSite.RunAsync();
 
-} else {
+}
+else
+{
 	builder.Build().Run();
 
 }
