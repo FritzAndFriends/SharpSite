d69c51da6037ed5d2db6fbcb2b1d25101af7af75
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2025-02-17 17:29:42 -0500

    Adding tests for creating posts

---
+++ e2e/SharpSite.E2E/CreatePostTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Playwright;
+
+namespace SharpSite.E2E;
+
+public class CreatePostTests : AuthenticatedPageTests
+{
+
+	// create a playwright test that logs in, navigates to the create post page, fills in the form and submits it
+	[Fact]
+	public async Task CreatePost()
+	{
+		await LoginAsDefaultAdmin();
+		await Page.GotoAsync("/admin/post");
+		await Page.GetByPlaceholder("Title").ClickAsync();
+		await Page.GetByPlaceholder("Title").FillAsync("Test Post");
+		await Page.GetByRole(AriaRole.Application).GetByRole(AriaRole.Textbox).FillAsync("This is a test");
+
+		await Page.ScreenshotAsync(new()
+		{
+			Path = "create_new_post.png",
+			FullPage = true,
+		});
+
+		await Page.GetByRole(AriaRole.Button, new() { Name = "Save" }).ClickAsync();
+
+		await Page.GotoAsync("/");
+		await Page.ScreenshotAsync(new()
+		{
+			Path = "testpost-home.png",
+			FullPage = true,
+		});
+
+		await Page.GetByRole(AriaRole.Link, new() { Name = "Test Post" }).ClickAsync();
+
+		await Page.ScreenshotAsync(new()
+		{
+			Path = "testpost.png",
+			FullPage = true,
+		});
+
+
+		var title = await Page.Locator("h1").InnerTextAsync();
+		Assert.Equal("Test Post", title);
+		await Logout();
+	}
+
+
+}
