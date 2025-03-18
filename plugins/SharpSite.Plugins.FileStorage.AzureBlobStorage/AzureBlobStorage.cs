4aef051a35d1d3a14a6472851d39347b71a95bb2
Tero Kilpeläinen <degenone1@gmail.com>
2025-03-18 13:55:59 +0200

    Initial testing

---
+++ plugins/SharpSite.Plugins.FileStorage.AzureBlobStorage/AzureBlobStorage.cs
@@ -0,0 +1,35 @@
+﻿using Azure.Storage.Blobs;
+using SharpSite.Abstractions.Base;
+using SharpSite.Abstractions.FileStorage;
+
+namespace SharpSite.Plugins.FileStorage.AzureBlobStorage;
+
+// https://learn.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-dotnet
+[RegisterPlugin(PluginServiceLocatorScope.Singleton, PluginRegisterType.FileStorage)]
+public class AzureBlobStorage(AzureBlobStorageConfigurationSection Configuration) : IHandleFileStorage
+{
+	public async Task<string> AddFile(FileData file)
+	{
+		if (string.IsNullOrEmpty(Configuration.ConnectionString)) throw new Exception();
+		BlobServiceClient blobServiceClient = new(Configuration.ConnectionString);
+		BlobContainerClient blobContainerClient = await blobServiceClient.CreateBlobContainerAsync("images");
+		BlobClient blobClient = blobContainerClient.GetBlobClient(file.Metadata.FileName);
+		await blobClient.UploadAsync(file.File, true);
+		throw new NotImplementedException();
+	}
+
+	public Task<FileData> GetFile(string filename)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<IEnumerable<FileMetaData>> GetFiles(int page, int filesOnPage, out int totalFilesAvailable)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task RemoveFile(string filename)
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ plugins/SharpSite.Plugins.FileStorage.AzureBlobStorage/AzureBlobStorageConfigurationSection.cs
@@ -0,0 +1,14 @@
+﻿using SharpSite.Abstractions.Base;
+
+namespace SharpSite.Plugins.FileStorage.AzureBlobStorage;
+
+public class AzureBlobStorageConfigurationSection : ISharpSiteConfigurationSection
+{
+	public string SectionName { get; } = "Azure Blob Storage";
+	public string? ConnectionString { get; set; }
+
+	public Task OnConfigurationChanged(ISharpSiteConfigurationSection? oldConfiguration, IPluginManager pluginManager)
+	{
+		throw new NotImplementedException();
+	}
+}
